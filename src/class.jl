cfa666ff0975fac6f9001e9a04a83f21e2935986
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-02-07 10:49:31 -0800

    Use dedicated members to track number of rows/cols. Added documentation for class, constructor, getters/setters.

--- src/class.jl
+++ src/class.jl
@@ -2,17 +2,48 @@ export SummarizedExperiment
 import DataFrames
 import DataStructures
 
+"""
+The `SummarizedExperiment` class is a Bioconductor container for matrix-like data with row and column annotations.
+Any number of matrices (also known as "assays") can be stored in the container, 
+provided they are assigned to unique names and all have the same extents for the first two dimensions.
+
+The row and column annotations are stored as `DataFrame`s, with number of rows equal to the number of assay rows and columns, respectively.
+Any number and type of columns may be present in each `DataFrame`.
+If no annotations are present, they should be empty `DataFrame`s with no columns.
+
+Each instance may also contain arbitrary metadata not associated with the rows or columns.
+"""
 mutable struct SummarizedExperiment
+    nrow::Int64
+    ncol::Int64
     assays::DataStructures.OrderedDict{String,AbstractArray}
     rowdata::DataFrames.DataFrame
     coldata::DataFrames.DataFrame
     metadata::Dict{String,Any}
 
+    @doc """
+        SummarizedExperiment(assays, rowdata=DataFrame(), coldata=DataFrame(), metadata=Dict{String,Any}())
+
+    Create an instance of a `SummarizedExperiment`.
+
+    ```jldoctest
+    julia> using DataFrames, DataStructures
+    julia> assays = OrderedDict{String, AbstractArray}(
+              "foobar" => [[1,2] [3,4] [5,6]], 
+              "whee" => [[1.2,2.3] [3.4,4.5] [5.6,7.8]])
+    julia> coldata = DataFrame(Treatment=["normal", "drug1", "drug2"])
+    julia> x = SummarizedExperiments.SummarizedExperiment(assays, DataFrame(), coldata)
+    2x3 SummarizedExperiments.SummarizedExperiment
+      rowdata(0):
+      coldata(1): Treatment
+      metadata(0):
+    ```
+    """
     function SummarizedExperiment(
             assays::DataStructures.OrderedDict{String, AbstractArray},
-            rowdata::Union{DataFrames.DataFrame,Vector{String},Nothing},
-            coldata::Union{DataFrames.DataFrame,Vector{String},Nothing},
-            metadata::Union{Dict{String,Any},Nothing}
+            rowdata::DataFrames.DataFrame=DataFrames.DataFrame(),
+            coldata::DataFrames.DataFrame=DataFrames.DataFrame(),
+            metadata::Dict{String,Any}=Dict{String,Any}()
         )
 
         if length(assays) < 1
@@ -23,20 +54,25 @@ mutable struct SummarizedExperiment
         if length(refdims) < 2 
             throw(DimensionMismatch("'assays' should contain arrays with 2 or more dimensions"))
         end
+        nrow = refdims[1]
+        ncol = refdims[2]
 
         first_name = first(assays).first
         for (key, val) in assays
-            check_assay_dims(size(val), refdims, "assays['" * repr(key) * "']", "assays['" * repr(first_name) * "']")
+            dims = size(val)
+            if length(dims) < 2 || dims[1] != nrow || dims[2] != ncol
+                throw(DimensionMismatch("'assays[" * repr(key) * "]' and 'assays[" * repr(first_name) * "]' should have the same extents for the first 2 dimensions"))
+            end
         end
 
-        _rowdata = create_input_DataFrame(refdims[1], rowdata, "rowdata")
-        _coldata = create_input_DataFrame(refdims[2], coldata, "coldata")
-
-        _metadata = Dict{String,Any}()
-        if isa(metadata, Dict{String,Any})
-            _metadata = metadata
+        if size(rowdata)[2] > 0 && size(rowdata)[1] != nrow
+            throw(DimensionMismatch("unexpected number of rows for 'rowdata"))
         end
 
-        new(assays, _rowdata, _coldata, _metadata)
+        if size(coldata)[2] > 0 && size(coldata)[1] != ncol
+            throw(DimensionMismatch("unexpected number of rows for 'coldata"))
+        end
+
+        new(nrow, ncol, assays, rowdata, coldata, metadata)
     end
 end
--- src/getters.jl
+++ src/getters.jl
@@ -1,65 +1,176 @@
-export size, rowdata, setrowdata, coldata, setcoldata, assay, setassay, assays, setassays, metadata, setmetadata
+export size, rowdata, setrowdata!, coldata, setcoldata!, assay, setassay!, assays, setassays!, metadata, setmetadata!
 import DataFrames
 import DataStructures
 
+"""
+    size(x::SummarizedExperiment)
+
+Return a 2-tuple containing the number of rows and columns in `x`.
+"""
 function Base.size(x::SummarizedExperiment)
-    return (size(x.rowdata)[1], size(x.coldata)[1])
+    return (x.nrow, x.ncol)
 end
 
+"""
+    rowdata(x)
+
+Return the row annotations as a `DataFrame` with number of rows equal to the number of rows in `x`.
+If no annotations are present, an empty `DataFrame` is returned instead.
+"""
 function rowdata(x::SummarizedExperiment)
     return x.rowdata
 end
 
-function setrowdata(x::SummarizedExperiment, value::DataFrames.DataFrame)
-    if size(value)[1] != size(x.rowdata)[1]
+"""
+    setrowdata!(x, value)
+
+Set the row annotations in `x` to `value`.
+This can either be an empty `DataFrame` (i.e., no row annotations)
+or a `DataFrame` with number of rows equal to the number of rows in `x`.
+"""    
+function setrowdata!(x::SummarizedExperiment, value::DataFrames.DataFrame)
+    if size(value)[2] > 0 && size(value)[1] != x.nrow
         throw(DimensionMismatch("'value' and 'rowdata(x)' should have the same number of rows"))
     end
     x.rowdata = value
-    return x
+    return
 end 
 
+"""
+    coldata(x)
+
+Return the column annotations as a `DataFrame` with number of rows equal to the number of columns in `x`.
+If no annotations are present, an empty `DataFrame` is returned instead.
+"""
 function coldata(x::SummarizedExperiment)
     return x.coldata
 end
 
-function setcoldata(x::SummarizedExperiment, value::DataFrames.DataFrame)
-    if size(value)[1] != size(x.coldata)[1]
+"""
+    setcoldata!(x, value)
+
+Set the column annotations in `x` to `value`.
+This can either be an empty `DataFrame` (i.e., no column annotations)
+or a `DataFrame` with number of rows equal to the number of columns in `x`.
+"""    
+function setcoldata!(x::SummarizedExperiment, value::DataFrames.DataFrame)
+    if size(value)[2] > 0 && size(value)[1] != x.ncol
         throw(DimensionMismatch("'value' and 'coldata(x)' should have the same number of rows"))
     end
     x.coldata = value
-    return x
+    return
 end 
 
+"""
+    assay(x[, i])
+
+Return the requested assay in `x`.
+`i` may be an integer specifying an index or a string containing the name.
+If `i` is not supplied, the first assay is returned.
+"""
 function assay(x::SummarizedExperiment)
-    if length(x.assays) < 1
-        throw(BoundsError("'x' has no assays"))
-    end
-    return first(x.assays).second
+    return assay(x, 1)
 end
 
+function assay(x::SummarizedExperiment, i::Int64)
+    counter = 0
+    for v in values(x.assays)
+        counter += 1
+        if counter == i
+            return v
+        end
+    end
+
+    throw(BoundsError("'i = " * string(i) * "' is out of range of 'assays(x)'"))
+end
+
+function assay(x::SummarizedExperiment, i::String)
+    if !haskey(x.assays, i)
+        throw(KeyError("'" * i * "' is not present in 'assays(x)'"))
+    end
+    return x.assays[i]
+end
+
+"""
+    setassay!(x[, i], value)
+
+Set the requested assay in `x` to `value`.
+The first two dimensions of `value` must have extent equal to those of `x`.
+`i` may be an integer specifying an index, in which case it must be positive and no greater than `length(assays(x))`;
+or a string containing the name, in which case it may be an existing or new name.
+If `i` is not supplied, `value` is set to the first assay.
+"""
+function setassay!(x::SummarizedExperiment, value::AbstractArray)
+    setassay!(x, 1, value)
+    return
+end
+
+function setassay!(x::SummarizedExperiment, i::Int64, value::AbstractArray)
+    dim = size(value)
+    if dim[1] != x.nrow || dim[2] != x.ncol
+        throw(DimensionMismatch("dimensions of 'value' should be the same as those of 'x'"))
+    end
+
+    counter = 0
+    for k in keys(x.assays)
+        counter += 1
+        if counter == i
+            x.assays[k] = value
+            return
+        end
+    end
+
+    throw(BoundsError("'i = " * string(i) * "' is out of range of 'assays(x)'"))
+end
+
+function setassay!(x::SummarizedExperiment, i::String, value::AbstractArray)
+    dim = size(value)
+    if dim[1] != x.nrow || dim[2] != x.ncol
+        throw(DimensionMismatch("dimensions of 'value' should be the same as those of 'x'"))
+    end
+    x.assays[i] = value
+    return
+end
+
+"""
+    assays(x)
+
+Return all assays from `x`.
+"""
 function assays(x::SummarizedExperiment)
     return x.assays
 end
 
-function setassay(x::SummarizedExperiment, name::String, value::AbstractArray)
-    check_assay_dims(size(value), size(x), "value", "x")
-    x.assays[name] = value
-    return x
-end
+"""
+    setassays!(x, value)
 
-function setassays(x::SummarizedExperiment, value::DataStructures.OrderedDict{AbstractArray})
+Set assays in `x` to `value`.
+All values in `value` should have the same extents for the first two dimensions.
+"""
+function setassays!(x::SummarizedExperiment, value::DataStructures.OrderedDict{String,AbstractArray})
     xdims = size(x)
     for (name, val) in value
         check_assay_dims(size(val), xdims, "value[" * repr(name) * "]", "x")
     end
     x.assays = value
-    return x
+    return
 end
 
+"""
+    metadata(x)
+
+Return all metadata from `x`.
+"""
 function metadata(x::SummarizedExperiment)
     return x.metadata;
 end
 
-function setmetadata(x::SummarizedExperiment, value::Dict{String,Any})
+"""
+    setmetadata!(x, value)
+
+Set metadata in `x` to `value`.
+"""
+function setmetadata!(x::SummarizedExperiment, value::Dict{String,Any})
     x.metadata = value;
+    return
 end
