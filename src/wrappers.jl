2f02d8943dbc3132858b15400ff40e8e4b0c9ef1
Giulio Benedetti <giuliobene2000@gmail.com>
2022-05-20 16:32:12 +0900

    Delete wrappers.jl

--- src/wrappers.jl
+++
@@ -1,8 +0,0 @@
-mat1 = ones(10, 10)
-mat2 = zeros(20, 5)
-assays = OrderedDict{String, AbstractArray}("matrix1" => mat1,
-                                            "matrix2" => mat2)
-
-function make_assays(kwargs...)
-
-    
